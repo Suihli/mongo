4628d613295dde08e1c713e28229cfe44c6fd124
Suihli <suihlimalek@gmail.com>
2024-10-14 13:50:45 +0100

    Premier commit

--- models/prodModel.js
+++ models/prodModel.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
     nom: { type: String, required: true },
     description: { type: String, required: true },
-    prix: { type: Number, required: true }
+    prix: { type: Number, required: true },
+    images: [{ type: String }] // Array of strings for image URLs
 });
 
 // Exporter le modèle de produit
